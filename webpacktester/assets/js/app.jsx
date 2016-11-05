4d515fa72e93ef53e22e3ab6f20c8958cf8dacf9
Adrian Montero <maplechori@gmail.com>
2016-11-05 22:58:10 +0400

    added more stuff, packages etc

--- webpacktester/assets/js/app.jsx
+++ webpacktester/assets/js/app.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import Match from 'react-router/Match'
+import Link from 'react-router/Link'
+import Redirect from 'react-router/Redirect'
+import Router from 'react-router/BrowserRouter'
 
 
-class App extends React.Component {
-  render() {
-    return <p>Hello, world!</p>;
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    cb()
+    setTimeout(cb, 100) // weird bug if async?
   }
 }
 
+const App = () => (
+  <Router>
+    {({ router }) => (
+      <div>
+        {fakeAuth.isAuthenticated ? (
+          <p>
+            Welcome! {' '}
+            <button onClick={() => {
+              fakeAuth.signout(() => {
+                router.transitionTo('/')
+              })
+            }}>Sign out</button>
+          </p>
+        ) : (
+          <p>You are not logged in.</p>
+        )}
+
+        <ul>
+          <li><Link to="/public">Public Page</Link></li>
+          <li><Link to="/protected">Protected Page</Link></li>
+        </ul>
+
+      </div>
+    )}
+  </Router>
+)
 export default App;
