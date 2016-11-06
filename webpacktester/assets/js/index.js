fae2b15a2aaa37f679c31fa94ea6a9ed8c7a0d34
Adrian Montero <maplechori@gmail.com>
2016-11-07 00:02:11 +0400

    still part of the material UI change

---
+++ webpacktester/assets/js/index.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react'
+import ReactDOM from 'react-dom'
+import Login from './login'
+import App from './app'
+import Layout from 'react-toolbox'
+
+function requireAuth(nextState, replace) {
+    if (!auth.loggedIn()) {
+        replace({
+            pathname:'/app/login/',
+            state: {nextPathname: '/app/'}
+        })
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('react-app'))
